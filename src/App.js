02bee45a7c66d133371fa63c6413a96fe334a21b
Vladimir A <avabramanc@gmail.com>
2020-01-12 22:06:38 +0300

    Connected 10 Lesson

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 //import El from './07_lists/Lesson';
 //import {Table} from './08_fragment_&_css/Lesson';
-import Form from './09_form/Lesson'; 
+import Form from './10_refs/Lesson'; 
 
 class App extends Component {
 

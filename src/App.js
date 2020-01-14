8550263e0e5bc627500e7010fc685eb0bf9661a6
Vladimir A <avabramanc@gmail.com>
2020-01-14 19:47:22 +0300

    Rewrite for lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 
-//import El from './07_lists/Lesson';
-//import {Table} from './08_fragment_&_css/Lesson';
-import Form from './10_refs/Lesson'; 
+
+import News from './14_work_api/containers/news';
 
 class App extends Component {
 
   render() {
     return (
-      <Form />
+      <News />
     );
   }
 }

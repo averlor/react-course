48091b8dfe737b2f9070e51f6fb5a4267a633a10
Vladimir Abramyants <abramyants@aurum.org>
2020-01-16 10:08:56 +0300

    Rewrite for lesson with animation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+import Lesson from './17_animations/Lesson';
 
-<<<<<<< HEAD
-import ToDo from './16_redux/container/todo';
-import Title from './16_redux/components/title/title';
-=======
-import Lesson from './16_todo/Lesson';
->>>>>>> a142cfd94003fec8ea594c09a3f94d4423b3bd34
 
 class App extends Component {
 
   render() {
     return (
-      <Fragment>
-          <Title title="ToDo App" />
-          <ToDo />
-      </Fragment>
+      <Lesson />
     );
   }
 }
